c7547037ce1b102ad111274718dce254bd58be74
Harsh Talreja <h28144871@gmail.com>
2026-06-23 15:47:17 +0530

    Added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import ProductCard from "../components/ProductCard";
+import { getProducts } from "../data/products"
+import {Link} from "react-router-dom"
+
 export default function Home(){
+    const products = getProducts();
     return <div className="page">
         <div className="home-hero">
             <h1 className="home-title">Welcome to ShopHub</h1>
@@ -6,7 +11,14 @@ export default function Home(){
         </div>
         <div className="container">
             <h2 className="page-title">Our Products</h2>
-            
+            <div className="product-grid">
+                {products.map((product) => (
+                    <ProductCard 
+                        key={product.id}
+                        product={product}
+                    />
+                ))}
+            </div>
         </div>
     </div>
 }
